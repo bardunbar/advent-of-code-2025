8152453832f489808e9980ca25f52a68ff78677b
bardunbar <61152068+bardunbar@users.noreply.github.com>
2025-12-03 09:39:20 +0000

    Day 1 - Part 2

--- day-1/app/Main.hs
+++ day-1/app/Main.hs
@@ -2,32 +2,42 @@ module Main where
 
 import System.Environment(getArgs)
 
-
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile
 
-parseCommand :: String -> Int
+parseCommand :: String -> (Int, Int)
 parseCommand command
-    | head command == 'R' = read (tail command)
-    | head command == 'L' = read (tail command) * (-1)
-parseCommand _ = 0
+    | head command == 'R' = (r, q)
+    | head command == 'L' = (r * (-1), q)
+    where
+        value = read (tail command)
+        (q, _) = quotRem value 100
+        (_, r) = divMod value 100
+parseCommand _ = (0, 0)
 
-getCommands :: [String] -> [Int]
+getCommands :: [String] -> [(Int, Int)]
 getCommands commands = do
     map parseCommand commands
 
-spinTheDial :: Int -> [Int] -> Int
-spinTheDial 0 [] = 1  -- Exit out in the case of the empty list, this case handles when the last action landed on 0
-spinTheDial _ [] = 0  -- Exit out in the case of the empty list
-spinTheDial 0 commands = 1 + spinTheDial (head commands) (tail commands)
-spinTheDial n commands = spinTheDial ((n + head commands) `mod` 100) (tail commands)
+checkTheDial :: Int -> Int -> Int
+checkTheDial current change
+    | current == 0 = 0
+    | current + change > 99 = 1
+    | current + change <= 0 = 1
+checkTheDial _ _ = 0
+
+spinAndCheck :: Int -> [(Int, Int)] -> Int
+spinAndCheck _ [] = 0
+spinAndCheck current ((v, extra):rest) =
+    checkTheDial current v + extra + spinAndCheck ((current + v) `mod` 100) rest
 
 main :: IO ()
 main = do
+    putStrLn "Advent of Code - Day 1 - Part 2"
     args <- getArgs
     case length args of
         0 -> putStrLn "Usage: Pass in the name of the file to parse!"
         _ -> let file = head args in do
             x <- readLines file
-            putStrLn ("Found some numbers: " ++ show (spinTheDial 50 (getCommands x)))
+            putStrLn ("Found some numbers: " ++ show (spinAndCheck 50 (getCommands x)))
 
