c3e1a1471581b25b96a087f9701aee412b837fea
bardunbar <61152068+bardunbar@users.noreply.github.com>
2025-12-10 14:00:04 +0000

    Day 5 - Part 2

--- day-5/app/Main.hs
+++ day-5/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Environment(getArgs)
 import Data.List.Split
+import Data.Maybe
 
 -- Read in all the lines of the file at FilePath to a list of strings
 readLines :: FilePath -> IO [String]
@@ -40,6 +41,36 @@ countFreshIngredients _ [] = 0
 countFreshIngredients f (cur:rest) =
     if any (testIngredient cur) f then 1 + countFreshIngredients f rest else countFreshIngredients f rest
 
+doesRangeOverlap :: (Int, Int) -> (Int, Int) -> Bool
+doesRangeOverlap (a, da) (b, db) =
+    let a_max = a + da in
+        let b_max = b + db in
+            a <= b && a_max >= b || a <= b_max && a_max >= b_max ||
+            b <= a && b_max >= a || b <= a_max && b_max >= a_max
+
+combineRanges :: (Int, Int) -> (Int, Int) -> (Int, Int)
+combineRanges (a, da) (b, db) =
+    let a_max = a + da in
+        let b_max = b + db in
+            let (cm, cx) = (min a b, max a_max b_max) in
+                (cm, cx - cm)
+
+-- Magic find function using the power of lazy evaluation
+-- listToMaybe turns the first element of a list into a Maybe, otherwise Nothing. Filter returns a list of all elements matching predicate
+-- Because of lazy evaluation, since we only need the head of the filtered list, filter will stop when it encounters the first hit!
+find :: (a -> Bool) -> [a] -> Maybe a
+find p = listToMaybe . filter p
+
+optimizeRanges :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
+optimizeRanges validRanges [] = validRanges
+optimizeRanges [] (oh:ot) = optimizeRanges [oh] ot
+optimizeRanges validRanges (oh:ot) =
+    case find (doesRangeOverlap oh) validRanges of
+        Just found -> let combined = combineRanges oh found in
+            optimizeRanges (filter (/=found) validRanges) (combined:ot)
+        Nothing -> optimizeRanges (oh:validRanges) ot
+
+
 main :: IO ()
 main = do
     putStrLn "Advent of Code - Day 5"
@@ -51,7 +82,11 @@ main = do
             case splitOn [""] rawLines of
                 (ranges:rawIngredients:_) -> do
                     let freshIngredients = getOffsetRange ranges
+                    let optimized = optimizeRanges [] freshIngredients
+                    let freshIngredientCount = foldr (\(_, d) a -> a + d + 1) 0 optimized
                     let ingredients = map read rawIngredients
-                    putStrLn("Result: " ++ show (countFreshIngredients freshIngredients ingredients))
+
+                    putStrLn("Possible Fresh: " ++ show freshIngredientCount)
+                    putStrLn("Result: " ++ show (countFreshIngredients optimized ingredients))
 
                 _ -> putStrLn "Error with input"
